1ca34b4152b88ba967adc59210478a08428dd613
jama mohamed abdi <j.m.abdi@hotmail.com>
2024-10-29 14:49:48 +0100

    la till class

---
+++ GitTraining/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTraining
+{
+    public class MyClass
+
+    {
+        public string Grupp7 { get; set; }
+
+        public double Rank {  get; set; }
+    }
+ 
+}
