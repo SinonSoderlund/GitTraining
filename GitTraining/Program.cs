aa6d725e3da60d22588d2ad4922a14ee416e404b
dansaa8 <dansaa8@protonmail.com>
2024-10-29 15:31:14 +0100

    Add cw msg

--- GitTraining/Program.cs
+++ GitTraining/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("daniel");
             Console.WriteLine("yes");
             Console.WriteLine(new Bunnyficator(args, 12).ToString());
+            Console.WriteLine("Update from dansaa8 branch");
         }
     }
 }
