40000dbf8991d411118b0ca641c345e5b5d58c36
SinonSoderlund <sinonlynx@gmail.com>
2024-10-29 14:38:11 +0100

    added bunnyficator, the class that turns objects to bunnies

---
+++ GitTraining/Bunnyficator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTraining
+{
+    internal class Bunnyficator
+    {
+        public string Name { get; set; }
+        public double Roundness { get; set; }
+        public Bunnyficator(object o, double roundess) 
+        {
+            Name = o.GetType().ToString();
+            Roundness = roundess;
+        }
+    }
+}
